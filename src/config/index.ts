2694dde25c734d6e6e3991ae7eb2b12bfb376a4d
Kenbaz <kbassey016@gmail.com>
2024-06-30 02:03:26 +0100

    hosted project on firebase

---
+++ src/config/index.ts
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDZNKx-bDfkGial61011A6m9WkGXTg4Zbc",
+  authDomain: "get-repo-dbc27.firebaseapp.com",
+  projectId: "get-repo-dbc27",
+  storageBucket: "get-repo-dbc27.appspot.com",
+  messagingSenderId: "582355693782",
+  appId: "1:582355693782:web:53bdac47d6dec2e3b79977",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
